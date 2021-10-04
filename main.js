18202e59ff0de90e83089562462283fa1e47c4f9
PetarR21 <petar.r.rakocevic@gmail.com>
2021-10-04 14:56:00 +0200

    Commit 1

---
+++ main.js
@@ -0,0 +1,25 @@
+
+let table = document.getElementById('table');
+let cells = Array.of(table.childNodes);
+
+//Function for creating cells width specified id,height and width
+function createCell(id, w, h) {
+    let cell = document.createElement('div');
+    cell.style.backgroundColor = 'white';
+    cell.style.width = w + "px";
+    cell.style.height = h + "px";
+    cell.id = id;
+
+    return cell;
+}
+
+//Create grid
+for (let i = 0; i < 16; i++) {
+    for (let j = 0; j < 16; j++) {
+        let cell = createCell(i, 37.5, 37.5);
+        table.appendChild(cell);
+    }
+}
+
+/*Add event listener on cells*/
+console.log(cells)
